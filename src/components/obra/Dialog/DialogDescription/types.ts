cbc5712a8f1cb8ef6f909aecd257c0c34cb4d554
Ali Shouman <alyshouman@hotmail.com>
2026-03-27 20:46:19 +0200

    deleted all components

--- src/components/obra/Dialog/DialogDescription/types.ts
+++
@@ -1,9 +0,0 @@
-import * as RadixDialog from '@radix-ui/react-dialog';
-import { ComponentPropsWithoutRef } from 'react';
-
-export interface DialogDescriptionProps extends ComponentPropsWithoutRef<typeof RadixDialog.Description> {
-  /**
-   * Additional CSS classes
-   */
-  className?: string;
-}
--- src/components/obra/Dialog/DialogTitle/types.ts
+++
@@ -1,9 +0,0 @@
-import * as RadixDialog from '@radix-ui/react-dialog';
-import { ComponentPropsWithoutRef } from 'react';
-
-export interface DialogTitleProps extends ComponentPropsWithoutRef<typeof RadixDialog.Title> {
-  /**
-   * Additional CSS classes
-   */
-  className?: string;
-}
