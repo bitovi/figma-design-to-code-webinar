89a779d14605c8cd7e0c6eae0dcbdc780b7c65ae
Ali Shouman <alyshouman@hotmail.com>
2026-04-19 23:27:36 +0200

    fix button

--- src/components/obra/Button/types.ts
+++ src/components/obra/Button/types.ts
@@ -2,15 +2,9 @@ import type React from 'react';
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary' | 'outline' | 'ghost' | 'destructive';
-<<<<<<< Updated upstream
   size?: 'lg' | 'md' | 'sm' | 'xs';
   roundness?: 'default' | 'round';
   leftIcon?: React.ReactNode;
   rightIcon?: React.ReactNode;
-=======
-  size?: 'mini' | 'sm' | 'default' | 'lg';
-  roundness?: 'default' | 'round';
-  children?: React.ReactNode;
->>>>>>> Stashed changes
   className?: string;
 }
